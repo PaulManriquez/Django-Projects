aa0ef310703a5e8740f620b5608e151fa2f5f7dd
Paul Manriquez <paulmanriquezengineer@gmail.com>
2024-07-23 16:48:18 -0600

    Contact app section complete, enable to send an email to the store

--- MiniMarketPaul_Django/MiniMarket/ContactApp/views.py
+++ MiniMarketPaul_Django/MiniMarket/ContactApp/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import FormContact
+from .forms import FormContact 
+from django.core.mail import EmailMessage
 # Create your views here.
 def Contact(request):
     Form_Contacts = FormContact() #Object of FormContact 
+    
+    if request.method == 'POST':
+        Form_Contacts = FormContact(data=request.POST) #Get the data from the post request
+        if Form_Contacts.is_valid(): # <-- Check all the data was filled 
+            Name = request.POST.get('Name')
+            Email = request.POST.get('Email')
+            Content = request.POST.get('Content')
+
+            Email_Message =  EmailMessage('This is a massage from django',
+                                          'The user: {} with Email:{} wrote this:\n{}'.format(Name,Email,Content),
+                                          '',['paulmanriquezengineer@gmail.com'],reply_to=[Email])
+            try:
+                Email_Message.send()
+
+                return redirect('/Contacts/?valid') #<--redirect with a parameter is like a flag 
+                                                    #to tell to the 'html' that is a post request
+            except:                                        
+                return redirect('/Contacts/?novalid')
+
     return render(request,'ContactApp/contact.html',{'Form_Contact':Form_Contacts})
--- MiniMarketPaul_Django/MiniMarket/MiniMarket/settings.py
+++ MiniMarketPaul_Django/MiniMarket/MiniMarket/settings.py
@@ -129,3 +129,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media') #<-- Tell to Django where to search 
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#EMAIL SEND | Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True 
+EMAIL_PORT = 587 
+EMAIL_HOST_USER = 'paulmanriquezengineer@gmail.com'
+EMAIL_HOST_PASSWORD = ''
